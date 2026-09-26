7b36c6cb64aa05781cfe8d93c96d7debedf3e341
Stefan Leustean <stefanleustean56@gmail.com>
2026-09-26 14:08:33 +0300

    Open the Ctrl-/ terminal as a floating window with a rounded border
    Machine: omarchy

---
+++ nvim/lua/plugins/snacks-terminal-float.lua
@@ -0,0 +1,8 @@
+return {
+	"folke/snacks.nvim",
+	opts = {
+		terminal = {
+			win = { position = "float", border = "rounded" }, -- Ctrl-/ opens a floating terminal instead of a bottom split
+		},
+	},
+}
