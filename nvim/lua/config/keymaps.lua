5c26d98e46a67e00b40fe1baf6d22a9ce7166719
Stefan Leustean <stefanleustean56@gmail.com>
2026-09-26 13:18:53 +0300

    Enable LazyVim's Python extra and add Space f y to copy the relative file path
    Machine: omarchy

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>fy", function()
+  vim.fn.setreg("+", vim.fn.expand("%"))
+end, { desc = "Copy relative file path" })
